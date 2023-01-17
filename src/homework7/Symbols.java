07c8b5179ac113a4ea3691f72030095d8b05e58a
cyber278a <cyber278a@gmail.com>
2023-01-17 17:00:38 -0500

    V2

---
+++ src/homework7/Symbols.java
@@ -0,0 +1,32 @@
+package homework7;
+
+public class Symbols {
+    public static void main(String[] args) {
+        for (int i =0; i <10
+                ; i++) {
+
+if(i%2==0){
+    System.out.println("*") ;}
+    System.out.println();}
+
+            
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+            // empty line
+
+                // inner for loop
+
+
+        /*if(i==6||i==9);
+        { System.out.println("**");
+            }*/
