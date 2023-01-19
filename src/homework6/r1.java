658255b0aab34021c62cee1faa4c0f091c867796
cyber278a <cyber278a@gmail.com>
2023-01-19 09:31:54 -0500

    V7

--- src/homework6/r1.java
+++ src/homework6/r1.java
@@ -2,8 +2,8 @@ package homework6;
 
 public class r1 {
     public static void main(String[] args) {
-        float a = 5f;
-        System.out.println("The multiplication of _a_ with itself is equal to" +a*a);
-
+        String Name="Java";
+        String Name2="programming language";
+        System.out.println(Name+" is the #1 "+Name2);
     }
 }
